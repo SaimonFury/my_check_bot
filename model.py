39328e3a5113748efe00accdc3094eb837e709e4
Natalia <n.platova@hotmail.com>
2019-06-30 13:38:12 +0700

    class Student

--- model.py
+++ model.py
@@ -2,5 +2,10 @@ from  flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), index=True, unique=True)
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self):
+        return f'<Student: {self.username}>'
